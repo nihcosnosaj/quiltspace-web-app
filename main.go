34a4798786fb5a457c422400e2c1f67087d57f15
Jason Sochin <jsochin8@gmail.com>
2022-01-31 15:18:50 -0800

    added individual quilt show feature

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"quiltspace/config"
@@ -21,17 +22,27 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", index)
 	router.GET("/quilts", quiltsIndex)
+	router.GET("/quilts/show", quiltsShow)
 	http.ListenAndServe(":8080", router)
 }
 
+// quiltsIndex handles all requests to "/" and "/quilts" from the client. It displays an index
+// of all quilts in the database. It querys for all quilts, then creates a slice
+// of type Quilt in which to place each quilt after the row has been scanned and
+// partitioned accordingly.
 func quiltsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	// The quiltsIndex handler will handle all requests to "/" and "/quilts".
-	// It will display a formatted list of all quilts in the database.
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// query database for all quilts
 	rows, err := config.DB.Query("SELECT * FROM quilts;")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Scan each row of query and add each row to a slice of type Quilt
 	qlts := make([]Quilt, 0)
 	for rows.Next() {
 		qlt := Quilt{}
@@ -43,6 +54,38 @@ func quiltsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	}
 
 	config.TPL.ExecuteTemplate(w, "index.html", qlts)
+}
+
+// quiltsShow handles all requests to "/quilts/show" from the client. It displays a
+// single quilt based on the passed in quilt name, "name", from the FormValue of the
+// request.
+func quiltsShow(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	row := config.DB.QueryRow("SELECT * FROM quilts WHERE name = $1", name)
+
+	qlt := Quilt{}
+	err := row.Scan(&qlt.Qid, &qlt.Name, &qlt.Style, &qlt.Pattern)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "show.html", qlt)
+
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
--- main_test.go
+++ main_test.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func TestQuiltsIndex(t *testing.T) {
-	t.Run("return welcome string", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-		ps := httprouter.Params{}
 
-		quiltsIndex(response, request, ps)
+}
 
-		got := response.Body.String()
-		want := "Welcome to the Quiltspace!\n"
+func TestQuiltsShow(t *testing.T) {
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
 }
