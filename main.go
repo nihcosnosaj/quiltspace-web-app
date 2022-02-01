287a54214f3e0d770b669af8ca6e6292340a1489
Jason Sochin <jsochin8@gmail.com>
2022-02-01 06:40:16 -0800

    added creation feature for new quilts

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func main() {
 	router.GET("/", index)
 	router.GET("/quilts", quiltsIndex)
 	router.GET("/quilts/show", quiltsShow)
+	router.GET("/quilts/create", quiltsCreateForm)
+	router.POST("/quilts/create/process", quiltsCreateProcess)
 	http.ListenAndServe(":8080", router)
 }
 
@@ -65,14 +67,17 @@ func quiltsShow(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		return
 	}
 
+	// retrieve quilt name queried from request & make sure it isn't empty
 	name := r.FormValue("name")
 	if name == "" {
 		http.Error(w, http.StatusText(400), http.StatusBadRequest)
 		return
 	}
 
+	// query the database for the requested quilt
 	row := config.DB.QueryRow("SELECT * FROM quilts WHERE name = $1", name)
 
+	// marshal query into type Quilt data structure for passing to template
 	qlt := Quilt{}
 	err := row.Scan(&qlt.Qid, &qlt.Name, &qlt.Style, &qlt.Pattern)
 	switch {
@@ -88,6 +93,48 @@ func quiltsShow(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 }
 
+// quiltsCreateForm simply brings up the form for entering information for a new
+// quilting project to be entered into the database. Once the "submit" button is
+// entered on the webpage, a call to quiltsCreateProcess actually processes the
+// insertion into the database.
+func quiltsCreateForm(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	config.TPL.ExecuteTemplate(w, "create.html", nil)
+}
+
+// quiltsCreateProcess handles inserting the information the user inputted into the form
+// on the webpage and validates the data. It then inserts it into the database and executes
+// a confirmation template to let the user know the quilt was successfully created.
+func quiltsCreateProcess(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get form values
+	qlt := Quilt{}
+	qlt.Name = r.FormValue("name")
+	qlt.Style = r.FormValue("style")
+	qlt.Pattern = r.FormValue("pattern")
+
+	// validate form values
+	if qlt.Name == "" || qlt.Style == "" || qlt.Pattern == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// insert values into database
+	var err error
+	_, err = config.DB.Exec("INSERT INTO quilts (name, style, pattern) VALUES ($1, $2, $3)", qlt.Name, qlt.Style, qlt.Pattern)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	// confirm insertion into database
+	config.TPL.ExecuteTemplate(w, "created.html", qlt)
+}
+
+// index redirects all requests to "/" to "/quilts"
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.Redirect(w, r, "/quilts", http.StatusSeeOther)
 }
