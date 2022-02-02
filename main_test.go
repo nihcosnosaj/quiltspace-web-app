52579876f7e0439c1cee6fd5fd45fcf896d15502
Jason Sochin <jsochin8@gmail.com>
2022-02-02 07:05:16 -0800

    added some unit test coverage, more to come

--- main_test.go
+++ main_test.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func TestQuiltsIndex(t *testing.T) {
+	wr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/quilts", nil)
+	ps := httprouter.Params{}
 
+	quiltsIndex(wr, req, ps)
+
+	if wr.Code != http.StatusOK {
+		t.Errorf("got http status code %d, expected 200", wr.Code)
+	}
+
+	if !strings.Contains(wr.Body.String(), "Quilt Index:") {
+		t.Errorf("Response body doesn't contain 'Quilt Index:")
+	}
 }
 
-func TestQuiltsShow(t *testing.T) {
+func TestQuiltsCreateForm(t *testing.T) {
+	wr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/quilts/create", nil)
+	ps := httprouter.Params{}
 
+	quiltsCreateForm(wr, req, ps)
+
+	if wr.Code != http.StatusOK {
+		t.Errorf("got http status code %d, expected 200", wr.Code)
+	}
+
+	if !strings.Contains(wr.Body.String(), "Create New Quilt") {
+		t.Errorf("response body doesn't contain 'Create New Quilt'")
+	}
 }
